ea89eb370ba89034c25120b5b42a82e0afecc90e
Seanovan Smith <sseanovan@yahoo.com>
2023-03-28 16:18:56 -0500

    reconstruction

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,27 @@
-// Define the API endpoint and API key
+// Variables
 const api = "https://api.openweathermap.org/data/2.5/forecast";
 const apiKey = "925c70324dece63089d9784a7f037499";
-
-// Get the search bar elements
 const searchBar = document.querySelector(".search-bar");
-const cityInput = searchBar.querySelector("input[type='text']:nth-of-type(1)");
-const countryInput = searchBar.querySelector("input[type='text']:nth-of-type(2)");
-const button = searchBar.querySelector("button");
-
-// Get the search history element
+const cityInput = searchBar.querySelector(".city-input");
+const countryInput = searchBar.querySelector(".country-input");
 const searchHistory = document.querySelector(".search-history");
-
-// Add an event listener to the button to fetch the forecast data when clicked
-button.addEventListener("click", () => {
+const btn = document.querySelector(".btn")
+// Click func.
+btn.addEventListener("click", function () {
   const city = cityInput.value;
   const country = countryInput.value;
   fetchForecast(city, country);
 });
-
-// Function to fetch the forecast data from the API
+// Fetch forecast
 function fetchForecast(city, country) {
   fetch(`${api}?q=${city},${country}&units=imperial&appid=${apiKey}`)
     .then(response => response.json())
     .then(data => {
-      // Extract the forecast data for the next 5 days
       const forecastData = data.list.slice(0, 5);
-
-      // Get the city name from the API data
       const cityName = data.city.name;
-
-      // Display the city name in the HTML
       const cityTitle = document.querySelector(".city-title");
       cityTitle.textContent = cityName;
 
-      // Loop through the forecast data and display it in the HTML
       forecastData.forEach((forecast, index) => {
         const forecastCard = document.getElementsByClassName("card")[index];
         const date = new Date(forecast.dt_txt);
@@ -52,9 +40,8 @@ function fetchForecast(city, country) {
         forecastCard.querySelector(".icon").setAttribute("src", `http://openweathermap.org/img/w/${weatherIcon}.png`);
       });
 
-      // Save the search query to localStorage
-      const searchQuery = `${city}, ${country}`;
-      searchQuery(searchQuery);
+      const searchHistory = localStorage.setItem( `${city}, ${country}`);
+       searchHistory(searchHistory);
 
     })
     .catch(error => console.error(error));
